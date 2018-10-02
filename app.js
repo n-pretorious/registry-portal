7710d5747915c5946c8d182d89c2e1ce9732b4f9
Pretorious Ndung'u <pretoriousndungu@gmail.com>
2018-10-02 13:28:21 +0300

    added mongodb and mongo js to handle post request and save the data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const path = require ('path');
-const { body } = require ('express-validator/check');
+const { body, validationResult } = require ('express-validator/check');
 const { sanitizeBody } = require ('express-validator/filter');
+const mongojs = require('mongojs');
+const db = mongojs('registry', ['users']);
 
 const app = express();
 
@@ -11,62 +13,61 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 // body-parse mildware
-app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post( '/users/add', (req, res) => {
-
-});
-
-//Set static path 
+//Set static path
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req,res) => {
-  res.render('index',{
-      title : 'Registration form'
+app.get('/', (req, res) => {
+  db.users.find(function (err, docs) {
+    // console.log(docs);
+    res.render('index',{
+      title : 'Registration form',
+      users : docs
+    });
   });
 });
 
-app.post('/users/add', [
+app.post('/signup', [
   body('email')
     .isEmail()
-    .not().isEmpty()
     .normalizeEmail(),
-  body('text')
-    .not().isEmpty(),
+  body('fname', 'lname')
+    .not().isEmpty().withMessage('This field cannot be empty')
+    .trim()
+    .escape(),
   sanitizeBody('notifyOnReply').toBoolean(),
+  // password must contain 8 characters and at least a number
   body('password')
-    .not().isEmpty()
-    .isLength({ min: 8 }).withMessage('must be at least 5 chars long')
+    .isLength({ min: 8 }).withMessage('must be at least 8 chars long')
     .matches(/\d/).withMessage('must contain a number'),
-  body('passwordConfirmation').custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error('Password confirmation does not match password');
-    }
-  }),
-  ], (req, res) => {
+    // checking if passwordConfirmation matches password
+  body('passwordConfirmation')
+    .custom((value, { req }) => value === req.body.password)
+], (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
+  } else {
+    let newUser = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirmation : req.body.passwordConfirmation
+    };
+    // insert users from the signup form
+    db.users.insert(newUser, function (err, result) {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect('/');
+    })
   }
-
-  User.create({
-    fname: req.body.fname,
-    lname: req.body.lname,
-    email: req.body.email,    
-    password: req.body.lname,
-    passwordConfirmation : req.body.passwordConfirmation
-    
-  }).then(user => res.json(user));
-
-  console.log(user);
-  res.render('home',{
-      userValue : user,
-      title : 'Registration form'
-  });
-   
 });
 
-app.listen(5000, () =>{
-  console.log('server running on port 5000');
+// local host that the server runs on
+app.listen(3000, () => {
+  console.log('server running on port 3000');
 })
